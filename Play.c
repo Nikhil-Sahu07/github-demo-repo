96b021d90e743d21931acaae9bf8cf843a45bea9
Nikhil Sahu <heynikhil07@gmail.com>
2023-03-22 22:03:48 +0530

    Take elements from user

---
+++ Play.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+    {
+        int i, j;
+        int arr[3][3];
+
+        for ( i = 0; i <3; i++)
+        {
+            printf("Enter numbers for the Row %d---\n",i+1);
+
+            // loop for elements of the row
+            for(j=0; j<3; j++)
+                {
+                    printf("Enter element %d:",j+1);
+                    scanf("%d",&arr[i][j]);
+                }
+        }
+        printf("Your data has been submitted successfully..!");
+        
+    }
